18871f9f4ff1394b03e121a2bbe93e84b0c350ff
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-05-31 04:04:10 +0900

    옵션 메뉴 수정

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Setting : MonoBehaviour
 
 {
 
     [SerializeField] GameObject objSetting;
+    [SerializeField] GameObject objKeySet;
     // Start is called before the first frame update
     void Start()
     {
         objSetting.SetActive(false);
+        objKeySet.SetActive(false);
     }
 
     // Update is called once per frame
@@ -32,5 +35,24 @@ public class Setting : MonoBehaviour
                 objSetting.SetActive(false);
             }
         }
+    }
+
+    public void MainonClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ResmueClick()
+    {
+        if(objSetting.activeSelf == true)
+        {
+            objSetting.SetActive(false);
+        }
+    }
+
+    public void OnclickSetting()
+    {
+        objSetting.SetActive(false);
+        objKeySet.SetActive(true );
     }
 }

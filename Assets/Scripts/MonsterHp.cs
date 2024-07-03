21d3a0c6b86deade5a3c0d5423765dcf52e9bc6c
DESKTOP-KPKDIHP\junpu <https://github.com/Junpyopak/plat>
2024-07-03 20:37:55 +0900

    몬스터 체력바 및 죽음

--- Assets/Scripts/MonsterHp.cs
+++ Assets/Scripts/MonsterHp.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class MonsterHp : MonoBehaviour
 {
-    private Image HpBar;
+    private Image Hp;
     [SerializeField] GameObject monster;
+    [SerializeField] float curHp = 5;
+    [SerializeField] float maxHp = 5;
     // Start is called before the first frame update
 
     private void Awake()
     {
-        HpBar = transform.Find("Hp").GetComponent<Image>();
+        Hp = transform.Find("Hp").GetComponent<Image>();
     }
     void Start()
     {
@@ -22,6 +24,8 @@ public class MonsterHp : MonoBehaviour
     void Update()
     {
         PosHpbar();
+        checkHp();
+        dearh();
     }
 
     private void PosHpbar()
@@ -29,7 +33,26 @@ public class MonsterHp : MonoBehaviour
         Vector3 hpPos = monster.transform.position;//몬스터의 위치, 월드 포지션
         //Camera.main.WorldToScreenPoint(hpPos);
         hpPos.x -= 0.31f;
-        hpPos.y += 0.91f;
+        hpPos.y += 0.93f;
         transform.position = hpPos;
+    }
+    private void checkHp()
+    {
+        float valueHp = curHp/maxHp;
+        if(Hp.fillAmount >valueHp) 
+        {
+            Hp.fillAmount -= Time.deltaTime;
+            if (Hp.fillAmount <= valueHp)
+            {
+                Hp.fillAmount = valueHp;
+            }
+        }
+    }
+    private void dearh()
+    {
+        if(Hp.fillAmount <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
